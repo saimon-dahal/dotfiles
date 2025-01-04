8da40439a5e5d1e66034aaddf3721a63f69bb976
SaimonDahal-02 <saimondahal66@gmail.com>
2025-01-04 10:53:22 +0545

    format change

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -1,14 +1,14 @@
 local opts = {
-	noremap = true,
-	silent = true,
+    noremap = true,
+    silent = true,
 }
 
 vim.api.nvim_create_autocmd("TextYankPost", {
-	desc = "Highlight when yanking (copying) text",
-	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
-	callback = function()
-		vim.highlight.on_yank()
-	end,
+    desc = "Highlight when yanking (copying) text",
+    group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+    callback = function()
+        vim.highlight.on_yank()
+    end,
 })
 
 -----------------
