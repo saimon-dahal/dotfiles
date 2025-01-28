66c3d8ce60785204ee9086c11cd271318caf9e4c
SaimonDahal-02 <saimondahal66@gmail.com>
2025-01-28 21:03:44 +0545

    make background of nvim transparent and other qol changes

--- nvim/lua/plugins/themes.lua
+++ nvim/lua/plugins/themes.lua
@@ -1,19 +1,22 @@
 -- Lua
 
 return {
-	"olivercederborg/poimandres.nvim",
-	lazy = false,
-	priority = 1000,
-	config = function()
-		require("poimandres").setup({
-			-- leave this setup function empty for default config
-			-- or refer to the configuration section
-			-- for configuration options
-		})
-	end,
+    "olivercederborg/poimandres.nvim",
+    lazy = false,
+    transparent = true,
+    priority = 1000,
+    config = function()
+        require("poimandres").setup({
+            bold_vert_split = true, -- use bold vertical separators
+            dim_nc_background = true, -- dim 'non-current' window backgrounds
+            disable_background = true, -- disable background
+            disable_float_background = true, -- disable background for floats
+            disable_italics = true, -- disable italics
+        })
+    end,
 
-	-- optionally set the colorscheme within lazy config
-	init = function()
-		vim.cmd("colorscheme poimandres")
-	end,
+    -- optionally set the colorscheme within lazy config
+    init = function()
+        vim.cmd("colorscheme poimandres")
+    end,
 }
