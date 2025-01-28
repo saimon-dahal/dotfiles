c3825f3493258251493b63fd5860bd4cbf7e44b8
SaimonDahal-02 <saimondahal66@gmail.com>
2025-01-28 20:40:15 +0545

    idk

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -4,6 +4,19 @@ return {
 		tag = "0.1.8",
 		dependencies = { "nvim-lua/plenary.nvim" },
 		config = function()
+			local telescope = require("telescope")
+			local actions = require("telescope.actions")
+
+			telescope.setup({
+				defaults = {
+					mappings = {
+						i = {
+							["<C-s>"] = actions.select_vertical,
+						},
+					},
+					selection_strategy = "reset",
+				},
+			})
 			local builtin = require("telescope.builtin")
 			vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
 			vim.keymap.set("n", "<leader>pg", builtin.live_grep, {})
