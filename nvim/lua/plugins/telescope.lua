663c2517f49ce4d8a57873060056b6ac6cf137a9
SaimonDahal-02 <saimondahal66@gmail.com>
2025-02-05 14:48:21 +0545

    include harpoon and show .gitignore file in telescope

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -16,9 +16,18 @@ return {
 					},
 					selection_strategy = "reset",
 				},
+				pickers = {
+					find_files = {
+						hidden = true,
+					},
+				},
 			})
 			local builtin = require("telescope.builtin")
-			vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
+			vim.keymap.set("n", "<leader>pf", function()
+				builtin.find_files({
+					find_command = { "rg", "--files", "--hidden", "--glob", "!.git/*" },
+				})
+			end, {})
 			vim.keymap.set("n", "<leader>pg", builtin.live_grep, {})
 			vim.keymap.set("n", "<leader>pb", builtin.buffers, {})
 			vim.keymap.set("n", "<leader>ph", builtin.help_tags, {})
