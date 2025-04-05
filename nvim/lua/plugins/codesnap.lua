4ce8d5bf87bd25e5ba8b54ae518170249806a09c
SaimonDahal-02 <saimondahal66@gmail.com>
2025-04-05 12:33:15 +0545

    add location for saving snapshots

--- nvim/lua/plugins/codesnap.lua
+++ nvim/lua/plugins/codesnap.lua
@@ -3,7 +3,7 @@ return {
     build = "make build_generator",
     keys = {
         { "<leader>cc", "<cmd>CodeSnap<cr>",     mode = "v", desc = "Save selected code snapshot into clipboard" },
-        { "<leader>cs", "<cmd>CodeSnapSave<cr>", mode = "v", desc = "Save selected code snapshot in ~/Pictures" },
+        { "<leader>cs", "<cmd>CodeSnapSave<cr>", mode = "v", desc = "Save selected code snapshot in ~/Pictures/codesnaps" },
     },
     opts = {
         save_path = "~/Pictures/codesnaps",
