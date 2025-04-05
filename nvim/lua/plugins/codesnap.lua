cc8bf362a1cebf9ed8a742f50107d7081fd8de36
SaimonDahal-02 <saimondahal66@gmail.com>
2025-04-05 12:10:32 +0545

    add: codesnap functionality

---
+++ nvim/lua/plugins/codesnap.lua
@@ -0,0 +1,14 @@
+return {
+    "mistricky/codesnap.nvim",
+    build = "make build_generator",
+    keys = {
+        { "<leader>cc", "<cmd>CodeSnap<cr>",     mode = "v", desc = "Save selected code snapshot into clipboard" },
+        { "<leader>cs", "<cmd>CodeSnapSave<cr>", mode = "v", desc = "Save selected code snapshot in ~/Pictures" },
+    },
+    opts = {
+        save_path = "~/Pictures/codesnaps",
+        has_breadcrumbs = true,
+        bg_theme = "sea",
+        watermark = "root"
+    },
+}
