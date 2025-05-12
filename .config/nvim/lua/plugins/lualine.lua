48d4b147fea48272f996d15a018102dbcdf3876d
SaimonDahal-02 <saimondahal66@gmail.com>
2025-05-12 21:01:23 +0545

    change theme to kanso and openlinks helper

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -6,7 +6,7 @@ return {
             require("lualine").setup({
                 options = {
                     icons_enabled = true,
-                    themes = "one_monokai",
+                    themes = "kanso",
                     component_separators = { left = "", right = "" },
                     section_separators = { left = "", right = "" },
                     disabled_filetypes = {
--- .config/nvim/lua/plugins/themes.lua
+++ .config/nvim/lua/plugins/themes.lua
@@ -1,18 +1,36 @@
+-- return {
+--     "cpea2506/one_monokai.nvim",
+--     name = 'one_monokai',
+--     lazy = false,
+--     priority = 1000,
+--     config = function()
+--         require("one_monokai").setup({
+--             transparent = true,
+--             colors = {},
+--             highlights = function(colors)
+--                 return {}
+--             end,
+--             italics = true,
+--         })
+--
+--         vim.cmd('colorscheme one_monokai')
+--     end,
+-- }
 return {
-    "cpea2506/one_monokai.nvim",
-    name = 'one_monokai',
+    "webhooked/kanso.nvim",
+    name = "kanso",
     lazy = false,
     priority = 1000,
     config = function()
-        require("one_monokai").setup({
+        require("kanso").setup({
             transparent = true,
-            colors = {},
-            highlights = function(colors)
-                return {}
-            end,
             italics = true,
+            theme = "zen", -- Load "zen" theme
+            background = { -- map the value of 'background' option to a theme
+                dark = "zen", -- try "ink" !
+            },
         })
 
-        vim.cmd('colorscheme one_monokai')
+        vim.cmd("colorscheme kanso")
     end,
 }
