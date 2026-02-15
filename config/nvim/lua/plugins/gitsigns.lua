cd7b91b819ebad373457bead2960f9b08a1f80df
Saimon Dahal <saimondahal@Saimons-MacBook-Pro.local>
2026-02-15 14:15:53 +1100

    remove: unnecessary folders for now

--- config/nvim/lua/plugins/gitsigns.lua
+++
@@ -1,7 +0,0 @@
-require("gitsigns").setup({
-	current_line_blame = true,
-	current_line_blame_opts = {
-		delay = 1000, -- milliseconds before showing blame (1 second)
-		virt_text_pos = "eol", -- or "overlay", "right_align"
-	},
-})
--- install/zdf-link-apps.sh
+++
@@ -1,6 +0,0 @@
-# Copy .desktop declarations
-mkdir -p ~/.local/share/applications
-cp ~/dotfiles/applications/*.desktop ~/.local/share/applications/
-cp ~/dotfiles/applications/hidden/*.desktop ~/.local/share/applications/
-
-update-desktop-database ~/.local/share/applications
